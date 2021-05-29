1f0f6d4fbb7af90be034e4773f4a56af3fea732d
Ilya Gulya <ilyagulya@gmail.com>
2021-05-29 18:45:03 +0300

    Converted Log to Kotlin

--- shared/src/main/java/org/jetbrains/skija/impl/Log.kt
+++ shared/src/main/java/org/jetbrains/skija/impl/Log.kt
@@ -1,86 +1,76 @@
-package org.jetbrains.skija.impl;
+package org.jetbrains.skija.impl
 
-import java.time.*;
-import java.time.format.*;
-import java.util.*;
-import java.util.function.*;
-import org.jetbrains.annotations.*;
+import java.time.ZoneOffset
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+import java.util.function.Supplier
 
-public class Log {
-    @ApiStatus.Internal
-    public static final int _level;
-
-    static { 
-        String property = System.getProperty("skija.logLevel");
-        if ("ALL".equals(property))
-            _level = 0;
-        else if("TRACE".equals(property))
-            _level = 1;
-        else if("DEBUG".equals(property))
-            _level = 2;
-        else if("INFO".equals(property))
-            _level = 3;
-        else if(null == property || "WARN".equals(property))
-            _level = 4;
-        else if("ERROR".equals(property))
-            _level = 5;
-        else if("NONE".equals(property))
-            _level = 6;
-        else
-            throw new IllegalArgumentException("Unknown log level: " + property);
+object Log {
+    internal var _level = 0
+    fun trace(s: String) {
+        _log(1, "[TRACE]", s)
     }
 
-    public static void trace(String s) {
-        _log(1, "[TRACE]", s);
+    fun debug(s: String) {
+        _log(2, "[DEBUG]", s)
     }
 
-    public static void debug(String s) {
-        _log(2, "[DEBUG]", s);
+    fun info(s: String) {
+        _log(3, "[INFO ]", s)
     }
 
-    public static void info(String s) {
-        _log(3, "[INFO ]", s);
+    fun warn(s: String) {
+        _log(4, "[WARN ]", s)
     }
 
-    public static void warn(String s) {
-        _log(4, "[WARN ]", s);
+    fun error(s: String) {
+        _log(5, "[ERROR]", s)
     }
 
-    public static void error(String s) {
-        _log(5, "[ERROR]", s);
+    fun trace(s: Supplier<String>) {
+        _log(1, "[TRACE]", s)
     }
 
-    public static void trace(Supplier<String> s) {
-        _log(1, "[TRACE]", s);
+    fun debug(s: Supplier<String>) {
+        _log(2, "[DEBUG]", s)
     }
 
-    public static void debug(Supplier<String> s) {
-        _log(2, "[DEBUG]", s);
+    fun info(s: Supplier<String>) {
+        _log(3, "[INFO ]", s)
     }
 
-    public static void info(Supplier<String> s) {
-        _log(3, "[INFO ]", s);
+    fun warn(s: Supplier<String>) {
+        _log(4, "[WARN ]", s)
     }
 
-    public static void warn(Supplier<String> s) {
-        _log(4, "[WARN ]", s);
+    fun error(s: Supplier<String>) {
+        _log(5, "[ERROR]", s)
     }
 
-    public static void error(Supplier<String> s) {
-        _log(5, "[ERROR]", s);
+    fun _time(): String {
+        return ZonedDateTime.now(ZoneOffset.UTC).format(DateTimeFormatter.ISO_INSTANT)
     }
 
-    public static String _time() {
-        return ZonedDateTime.now(ZoneOffset.UTC).format(DateTimeFormatter.ISO_INSTANT);
+    fun _log(level: Int, prefix: String, s: String) {
+        if (level >= _level) println(_time() + " " + prefix + " " + s)
     }
 
-    public static void _log(int level, String prefix, String s) {
-        if (level >= _level)
-            System.out.println(_time() + " " + prefix + " " + s);
+    fun _log(level: Int, prefix: String, s: Supplier<String>) {
+        if (level >= _level) println(_time() + " " + prefix + " " + s.get())
     }
 
-    public static void _log(int level, String prefix, Supplier<String> s) {
-        if (level >= _level)
-            System.out.println(_time() + " " + prefix + " " + s.get());
+    init {
+        val property = System.getProperty("skija.logLevel")
+        _level =
+            when (property) {
+                "ALL" -> 0
+                "TRACE" -> 1
+                "DEBUG" -> 2
+                "INFO" -> 3
+                null, "WARN" -> 4
+                "ERROR" -> 5
+                "NONE" -> 6
+                else -> throw IllegalArgumentException("Unknown log level: " + property)
+            }
     }
 }
